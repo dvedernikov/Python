5c1b5b7867e2dcdf54167550c5964355a5e22888
djeysen <djeysen@gmail.com>
2025-03-04 21:37:18 +0200

    Домашне завдання переробка 3 го завдання

--- lesson3/HomeWork3_3.py
+++ lesson3/HomeWork3_3.py
@@ -1,16 +1,15 @@
-numbers = [1, 2, 3, 4, 5, 6]
-
-# Якщо список порожній, повертаємо два порожні списки
-if not numbers:
+numbers = [1, 2, 3, 4, 5]
+if numbers:
+    mid = len(numbers) // 2
+    if len(numbers) % 2 != 0:
+        mid += 1
+    list1 = numbers[:mid]
+    list2 = numbers[mid:]
+else:
     list1 = []
     list2 = []
-else:
-    # Розділяємо список навпіл (ціле ділення довжини на 2)
-    mid = len(numbers) // 2
-    list1 = numbers[:mid]  # Перша половина
-    list2 = numbers[mid:]  # Друга половина
 
-print("Список 1:", list1)
-print("Список 2:", list2)
+print( list1)
+print(list2)
 result = [list1, list2]
 print(result)
--- lesson3/lesson_3.py
+++ lesson3/lesson_3.py
@@ -338,15 +338,15 @@
 # [1] => [[1], []]
 # [] => [[], []]
 
-# nums = []
-# middle_index = len(nums) // 2
-#
-# if len(nums) % 2 != 0:
-#     middle_index += 1
-#
-# part1 = nums[:middle_index]
-# part2 = nums[middle_index:]
-# print(part1)
-# print(part2)
-# result = [part1, part2]
-# print(result)
+nums = [1, 2, 3, 4, 5, 6]
+middle_index = len(nums) // 2
+
+if len(nums) % 2 != 0:
+    middle_index += 1
+
+part1 = nums[:middle_index]
+part2 = nums[middle_index:]
+print(part1)
+print(part2)
+result = [part1, part2]
+print(result)
